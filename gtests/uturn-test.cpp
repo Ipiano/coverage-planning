46534f7f7c48bc17b47eaf8247c43b0c99ba7d21
ipiano <andrew@stelterfamily.net>
2020-02-09 17:41:25 -0700

    Fixup uturn tests

--- gtests/uturn-test.cpp
+++ gtests/uturn-test.cpp
@@ -16,7 +16,7 @@ quantity::Radians toRad(const double degrees)
 
 TEST(CCPPTests, UTurnIs0WhenParallel)
 {
-    turn_cost::UShaped calculator;
+    turn_cost::UShaped calculator(1, 1, 1);
 
     double cost = calculator.calculateTurnCost(Segment2d{{0, 0}, {0, 1}}, toRad(90));
     EXPECT_NEAR(cost, 0, 0.001);
@@ -33,7 +33,7 @@ TEST(CCPPTests, UTurnIs0WhenParallel)
 
 TEST(CCPPTests, UTurnIsUndirectional)
 {
-    turn_cost::UShaped calculator;
+    turn_cost::UShaped calculator(1, 1, 1);
 
     double cost = calculator.calculateTurnCost(Segment2d{{0, 0}, {0, 1}}, toRad(90));
     EXPECT_NEAR(cost, 0, 0.001);
@@ -62,16 +62,16 @@ TEST(CCPPTests, UTurnIsUndirectional)
 
 TEST(CCPPTests, UTurnPerpendicularIsOnlyTurn)
 {
-    turn_cost::UShaped calculator;
+    turn_cost::UShaped calculator(1, 1, 1);
 
     Segment2d segment{{0, 0}, {0, 1}};
-    double cost = calculator.calculateTurnCost(segment, toRad(0));
+    double cost     = calculator.calculateTurnCost(segment, toRad(0));
     double expected = toRad(180).value() * double(boost::geometry::length(segment)) * std::sin(toRad(90).value()) / 4.;
 
     EXPECT_NEAR(cost, expected, 0.001);
 
-    segment = {{0, 0}, {1, 1}};
-    cost = calculator.calculateTurnCost(segment, toRad(-45));
+    segment  = {{0, 0}, {1, 1}};
+    cost     = calculator.calculateTurnCost(segment, toRad(-45));
     expected = toRad(180).value() * double(boost::geometry::length(segment)) * std::sin(toRad(90).value()) / 4.;
 
     EXPECT_NEAR(cost, expected, 0.001);
@@ -79,12 +79,11 @@ TEST(CCPPTests, UTurnPerpendicularIsOnlyTurn)
 
 TEST(CCPPTests, UTurnAtAngleMoreExpensiveThanPerpendicular)
 {
-    turn_cost::UShaped calculator;
+    turn_cost::UShaped calculator(1, 1, 1);
 
     Segment2d segment{{0, 0}, {0, 1}};
     EXPECT_GT(calculator.calculateTurnCost(segment, toRad(-45)), calculator.calculateTurnCost(segment, toRad(0)));
 
     segment = {{0, 0}, {1, 1}};
     EXPECT_GT(calculator.calculateTurnCost(segment, toRad(0)), calculator.calculateTurnCost(segment, toRad(-45)));
-
 }
